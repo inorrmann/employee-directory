d4f96f8caf9e1b6ea89539432588c4cc76e1970d
Ingrid N <inorrmann@gmail.com>
2020-01-02 16:48:30 -0800

    initial react app files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import the file that will have the container for the entire users' directory
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
